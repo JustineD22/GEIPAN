daa52b73a6d518e0b47bf94f29fd01f023d3cf90
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-02 16:03:46 +0100

    Ajout de l'onglet Login et l'onglet vers le formulaire d'inscription

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,8 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <li><a href="index.php?page=login">Login</a></li>
+            <li><a href="index.php?page=inscription">Inscription</a></li>
         </ul>
     </nav>
 </header>
