031a9a408d10abdc9288b671d478cb0798e2db92
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-02 12:02:12 +0100

    correction mineur, ajout des nom de département entre les balises options

--- functions/getStates.php
+++ functions/getStates.php
@@ -15,10 +15,10 @@ function getStates()
     $resultatState = $requeteState->fetchAll(PDO::FETCH_ASSOC);
     
         $html = "<select name='states[]'>";
-        for ($i = 0 ; $i < count($resultatState) ; $i++) {
-            
+        for ($i = 0 ; $i < count($resultatState) ; $i++) {   
             $html .= "<optgroup label='" . $resultatState[$i]['stateNumber'] . "'>";
-            $html .= "<option value='" . $resultatState[$i]['stateLabel'] . "'>";           
+            $html .= "<option value='" . $resultatState[$i]['stateLabel'] . "'>";
+            $html .= $resultatState[$i]['stateLabel'];          
             $html .= "</option>";
             $html .= "</optgroup>";
         }
